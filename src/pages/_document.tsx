3e367872e7daca26988ed613a3d49b6816aca0ae
sun <sun@portkey.finance>
2023-07-10 15:38:01 +0800

    feat:adding

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1"
+        />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="public/favicon.ico"
+        />
+      </Head>
+      <body>
+        <Main />
+      </body>
+    </Html>
+  );
+}
